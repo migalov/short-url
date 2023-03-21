54997886b1cce15fb6ce572aaa3f7f0e4b8dffe2
Michel Muniagiseny <mafri.michel@gmail.com>
2023-03-21 18:01:08 +0300

    Create Boost section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css';
+import Boost from './components/Boost';
 import Footer from './components/Footer';
 import Header from './components/Header';
 
@@ -7,6 +8,7 @@ export default function App() {
   return (
     <>
       <Header />
+      <Boost />
       <Footer />
     </>
   );
---
+++ src/components/Boost.jsx
@@ -0,0 +1,12 @@
+export default function Boost() {
+  return (
+    <section className="boost relative overflow-hidden">
+
+            <div className="container flex flex-col justify-center items-center h-[300px] md:h-[250px]">
+                <h2 className="mb-5 text-[28px] md:text-[40px] text-white font-bold text-center">Boost your links today</h2>
+                <button className="btn rounded-full px-12 py-3 font-bold">Get Started</button>
+            </div>
+        
+      </section>
+  )
+}
